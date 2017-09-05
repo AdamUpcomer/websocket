3176a4ab572759afb1e52d68ff74da29f1b95487
AdamUpcomer <sigma25@Dinos-Retina.local>
2017-09-05 16:16:47 -0700

    started web socket locally
    started web socket locally

---
+++ pbp.py
@@ -0,0 +1,25 @@
+import sys
+import websocket
+
+ABIOS_WS_LINK = "wss://api.abiosgaming.com/ws?type=dota&token="
+
+if len(sys.argv) != 2:
+    print("Please include the access token!")
+else:
+        def on_message(ws, msg):
+            print("Message Arrived:" + msg)
+
+        def on_error(ws, error):
+            print(error)
+
+        def on_close(ws):
+            print("Connection Closed")
+
+        def on_open(ws):
+           ws.send("Hello!")
+        websocket.enableTrace(True)
+        ws = websocket.WebSocketApp("ws://echo.websocket.org",
+                                    on_message=on_message,
+                                    on_error=on_error,
+                                    on_close=on_close)
+        ws.run_forever()
